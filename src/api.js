ee231bb784d7c4dba4db714a78bc73af8ca9922b
akshaygit2003 <akshaynema2003@gmail.com>
2024-12-11 23:40:50 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "953ddb6020msh39fb7d967ed7109p137a6ejsna88623338450", // rapid api key
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "9c1b402cc35bfd6eb91dd8c9b64df83e"; // openweather API key
